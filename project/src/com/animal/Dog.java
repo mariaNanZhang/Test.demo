f3b97d9d4b40b4e67abd87eeb9b5f8b4c675741f
Nan <maria.nan.zhang@gmail.com>
2016-03-26 11:52:34 +0000

    new change

---
+++ project/src/com/animal/Dog.java
@@ -0,0 +1,11 @@
+package com.animal;
+
+public class Dog extends Animal {
+
+	public void eat(){
+		System.out.println("Dog eat!");
+	}
+	public void watchDoor(){
+		System.out.println("Dog watch door!");
+	}
+}
---
+++ project/src/com/animal/Test.java
@@ -0,0 +1,19 @@
+package com.animal;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Animal o1 = new Animal();
+		Animal o2 = new Dog();
+		o1.eat();
+		o2.eat();
+		Animal o3 = new Cat();
+		o3.eat();
+		
+	}
+
+}
